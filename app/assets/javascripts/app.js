011fb560ec71ff74c2bf51ec81380f2bd83d177c
lgausman1 <l.gausman@comcast.net>
2015-07-16 12:59:11 -0700

    infowindow on map marker

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -8,53 +8,62 @@ function initialize() {
 
 	var map = new google.maps.Map(document.getElementById('map-div'), mapOptions);
 	
-function codeAddresses () {
 	 $.get('/posts.json').
    	   done(function (data) {
   		//console.log(data);
-  		var addressArray = [];
-  		var titleArray = [];
-  		for(var i = 0; i < data.length; i++) {
-  			addressArray.push(data[i].location);
-  			titleArray.push(data[i].title);
-  		} // end for loop
-  		$.each(addressArray, function (index, address) {
-  			//console.log(index + ": " + address);
+
+  		$.each(data,function (ind, obj) {
+  			//obj.location, obj.status, obj.title, obj.id
+  			  
+  			function create_marker (MapPos) {
+  				var marker = new google.maps.Marker({
+  					position: MapPos,
+        			map: map
+  				});
+
+
+
+	  			// pass in the status, title, and link  !!!!! marker not defined !!!!
+	  			google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+	  				return function () {
+	  					infowindow.setContent(content);
+	  					infowindow.open(map, marker);
+	  				} // end return function
+	  			})
+	  			(marker, content, infowindow)); // end listener 
+
+
+
+  			} // end create_marker
+
+  			var infowindow = new google.maps.InfoWindow();
+
+  			var content = "<div>" + obj.status + 
+  								"<br>" + obj.title + 
+  								"<br>" + obj.id +
+  								"</div>";
+
+  			console.log(content);
+
+  			// geocode the address and set the marker 			
   			geocoder.geocode({
-  				'address': address + " San Francisco, CA"
-  			}, function(results, status) {
-  				if(status == google.maps.GeocoderStatus.OK) {
-  					var marker = new google.maps.Marker({
-  						position: results[0].geometry.location,
-  						map: map
-  					}); // end map				
-  				}
-  				else {
-  					console.log("Geocode was not successful: " + status);
-  				} // end if
-  			}); // end geocoder
-  		}); // end each addressArray
+  				'address': obj.location + " San Francisco, CA"
+  			}, function (results) {
+  				//call marker and pass in values
+  				myPos = results[0].geometry.location;
+  				create_marker(myPos);
+  				
+  			} // end function
 
-  		$.each(titleArray, function (index, title) {
-			 var infowindow = new google.maps.InfoWindow({
-			 	content: title
-			 });
+  			); // end geocode
 
 
-  		}); // end each titleArray
+  		}); // end each
   		
    	}); // end done
-}
 
-codeAddresses();
 
 }//end initialize
 
- // google.maps.event.addListener(marker, 'mouseover', (function(marker) { // marker is not defined
- //    return function() {
- //        var content = title;
- //        infowindow.setContent(content);
- //        infowindow.open(map, marker);
- //    }
- //  })(marker)); // end event listener
+ 
    	  
